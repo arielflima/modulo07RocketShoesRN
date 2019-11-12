447d6fb39a7caf97e9d8ab0771a7f3b3b514e54f
Ariel Lima <arielflima@gmail.com>
2019-11-12 02:27:01 -0300

    creating the project, configuring eslint, prettier, git, editorConfig, react navigation

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import { Container, ProductView } from './styles';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Container>
+        <ProductView></ProductView>
+      </Container>
+    );
+  }
+}
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+export const ProductView = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
